8774a41c42adb084d7198b16c6e2d928ef0741b0
Marlon Henrique de Souza <marlon.souuza@gmail.com>
2021-06-14 22:52:09 -0300

    first commit

---
+++ src/main/java/ueh/marlon/personapidio/PersonapiDioApplication.java
@@ -0,0 +1,13 @@
+package ueh.marlon.personapidio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonapiDioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonapiDioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ueh/marlon/personapidio/PersonapiDioApplicationTests.java
@@ -0,0 +1,13 @@
+package ueh.marlon.personapidio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonapiDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
