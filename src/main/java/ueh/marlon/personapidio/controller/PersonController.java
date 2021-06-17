d9a5a3e404e88426c3ba07c7c9cc1e344674c4ef
Marlon Henrique de Souza <marlon.souuza@gmail.com>
2021-06-17 00:22:43 -0300

    Create listAll method

--- src/main/java/ueh/marlon/personapidio/controller/PersonController.java
+++ src/main/java/ueh/marlon/personapidio/controller/PersonController.java
@@ -1,11 +1,16 @@
 package ueh.marlon.personapidio.controller;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import ueh.marlon.personapidio.dto.request.PersonDTO;
@@ -24,8 +29,13 @@ public class PersonController {
 	}
 
 	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
 	public MessageResponseDTO create(@Valid @RequestBody PersonDTO person) {
 		return personService.save(person);
 	}
 	
+	@GetMapping
+	public List<PersonDTO> listAll(){
+		return personService.listAll();
+	}
 }
